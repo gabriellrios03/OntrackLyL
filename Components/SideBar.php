9fa69beb96fb20490dda922513febf7d0a080190
Luis Gabriel Rios Sauceda <luisgabriel.riossauceda@ext.cemex.com>
2025-03-31 14:44:18 -0600

    Add Customer Controllers

--- Components/SideBar.php
+++ Components/SideBar.php
@@ -2,7 +2,7 @@
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-dark opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
       <a class="navbar-brand px-4 py-3 m-0" href="#">
-        <img src="../assets/img/logo-ct-dark.png" class="navbar-brand-img" width="26" height="26" alt="main_logo">
+        <img src="http://localhost:5500/assets/img/logo-ct-dark.png" class="navbar-brand-img" width="26" height="26" alt="main_logo">
         <span class="ms-1 text-sm text-dark">LYL - ONTRACK</span>
       </a>
     </div>
@@ -10,7 +10,7 @@
     <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active bg-gradient-dark text-white" href="../pages/home.php">
+          <a class="nav-link active bg-gradient-dark text-white" href="http://localhost:5500/pages/home.php">
             <i class="material-symbols-rounded opacity-5">home</i>
             <span class="nav-link-text ms-1">Home</span>
           </a>
@@ -20,13 +20,13 @@
           <h6 class="ps-4 ms-2 text-uppercase text-xs text-dark font-weight-bolder opacity-5">Operación</h6>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-dark" href="../pages/pre-viajes.php">
+          <a class="nav-link text-dark" href="http://localhost:5500/pages/pre-viajes.php">
             <i class="material-symbols-rounded opacity-5">schedule</i>
             <span class="nav-link-text ms-1">PreViajes</span>
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-dark" href="../pages/viajes.php">
+          <a class="nav-link text-dark" href="http://localhost:5500/pages/viajes.php">
             <i class="material-symbols-rounded opacity-5">directions_car</i>
             <span class="nav-link-text ms-1">Viajes</span>
           </a>
@@ -36,25 +36,25 @@
           <h6 class="ps-4 ms-2 text-uppercase text-xs text-dark font-weight-bolder opacity-5">Catálogos</h6>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-dark" href="../pages/clientes.php">
+          <a class="nav-link text-dark" href="http://localhost:5500/pages/clientes.php">
             <i class="material-symbols-rounded opacity-5">group</i>
             <span class="nav-link-text ms-1">Clientes</span>
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-dark" href="../pages/trucks.php">
+          <a class="nav-link text-dark" href="http://localhost:5500/pages/trucks.php">
             <i class="material-symbols-rounded opacity-5">local_shipping</i>
             <span class="nav-link-text ms-1">Trucks</span>
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-dark" href="../pages/equipo.php">
+          <a class="nav-link text-dark" href="http://localhost:5500/pages/equipo.php">
             <i class="material-symbols-rounded opacity-5">build</i>
             <span class="nav-link-text ms-1">Equipo</span>
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-dark" href="../pages/rutas.php">
+          <a class="nav-link text-dark" href="http://localhost:5500/pages/rutas.php">
             <i class="material-symbols-rounded opacity-5">map</i>
             <span class="nav-link-text ms-1">Rutas</span>
           </a>
@@ -64,7 +64,7 @@
           <h6 class="ps-4 ms-2 text-uppercase text-xs text-dark font-weight-bolder opacity-5">Drivers</h6>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-dark" href="../pages/drivers.php">
+          <a class="nav-link text-dark" href="http://localhost:5500/pages/drivers.php">
             <i class="material-symbols-rounded opacity-5">person</i>
             <span class="nav-link-text ms-1">Drivers</span>
           </a>
@@ -74,11 +74,11 @@
           <hr class="horizontal dark my-2">
         </li>
         <li class="nav-item">
-          <a class="nav-link text-danger" href="../Controllers/logout.php">
+          <a class="nav-link text-danger" href="http://localhost:5500/Controllers/logout.php">
             <i class="material-symbols-rounded opacity-5">logout</i>
             <span class="nav-link-text ms-1">Logout</span>
           </a>
         </li>
       </ul>
     </div>
-  </aside>
+</aside>
